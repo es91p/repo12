3af14d4c7835a61e8968ee586a59193ca2facdfd
Eswara Prasad <esospree@gmail.com>
2020-12-17 11:42:11 +0530

    Another project is added to repo12

---
+++ GitRepo11CloneTest19/src/com/git/test/HelloGitEclipseJava11.java
@@ -0,0 +1,12 @@
+package com.git.test;
+
+public class HelloGitEclipseJava11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to Git eclipse java world...");
+
+	}
+
+}
